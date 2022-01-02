f4a16713c058a83e3cdfc37d274345f8465a3ca6
debarya <debarya.ghosh1234@gmail.com>
2022-01-02 23:39:38 +0530

    added counter sprites

--- player.py
+++ player.py
@@ -1,10 +1,18 @@
 from turtle import Turtle
+import turtle 
 
-# -229.0, -150.0
-# -226.0 -200.0
+counter_sprite_1 = "./graphic/counter_1.gif"
+counter_sprite_2 = "./graphic/counter_2/gif"
+turtle.addshape(counter_sprite_1)
+turtle.addshape(counter_sprite_2)
+
 class Player(Turtle):
-    def __init__(self, x_pos, y_pos, color, name):
+    def __init__(self, x_pos, y_pos, color, name, sprite):
         super().__init__()
+        if sprite == 1:
+            self.shape(counter_sprite_1)
+        elif sprite == 2:
+            self.shape(counter_sprite_2)
         self.shape("circle")
         self.color(color)
         self.penup()
@@ -14,7 +22,4 @@ class Player(Turtle):
         self.name = name
 
     def move_anim(self, x_cor_list, y_cor_list):
-        # print(x_cor_list)
-        # print(y_cor_list)
-        # print("\n\n")
         self.goto(int(x_cor_list), int(y_cor_list))
